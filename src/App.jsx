f15337d65487a5e1645bf0614ffdfdd45046221e
Naiyar Azam <98547126+Imsultan786@users.noreply.github.com>
2022-09-08 23:34:13 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+import './index.css';
+
+const App=()=>{
+    let newTime = new Date().toLocaleTimeString();
+
+    const [ctime, setCtime] = useState(newTime);
+
+    const UpdateTime =() => {
+        newTime = new Date().toLocaleTimeString();
+        setCtime(newTime);
+    }
+
+    return(
+        <>
+        <h1>{ctime}</h1>
+        <button onClick={UpdateTime}>Get Time</button>
+        </>
+    );
+}
+
+export default App;
